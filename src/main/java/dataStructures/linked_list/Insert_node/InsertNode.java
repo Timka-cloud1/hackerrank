4562450867bb40485627370d717877bc9a454c1c
Timka-cloud <timka_amanzhol@mail.ru>
2022-05-26 19:06:10 +0600

    a

--- src/main/java/dataStructures/linked_list/Insert_node/InsertNode.java
+++ src/main/java/dataStructures/linked_list/Insert_node/InsertNode.java
@@ -5,6 +5,8 @@ import dataStructures.linked_list.print_linked_list.PrintLinkedList;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -272,39 +274,117 @@ public class InsertNode {
      * solve
      */
 
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode temp1=head1;
+        SinglyLinkedListNode temp2=head2;
+        List<SinglyLinkedListNode>list=new ArrayList<SinglyLinkedListNode>();
+        while(temp1!=null){
+            list.add(temp1);
+            temp1=temp1.next;
+        }
+        while(temp2!=null){
+            if(list.contains(temp2)){
+                break;
+            }
+            temp2=temp2.next;
+        }
+        return temp2.data;
+
+    }
+
+
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
 
-        SinglyLinkedList head = new SinglyLinkedList();
 
-        head.insertNode(9);
-        head.insertNode(10);
-        head.insertNode(11);
-        SinglyLinkedList head2 = new SinglyLinkedList();
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        head2.insertNode(3);
-        head2.insertNode(1);
-        head2.insertNode(11);
-        head2.insertNode(15);
-        head2.insertNode(18);
-        head2.insertNode(1);
-        head2.insertNode(0);
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            int index = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        SinglyLinkedListNode singlyLinkedListNode = mergeLists(head.head, head2.head);
-        printLinkedList(singlyLinkedListNode);
-//        llist.insertNode(9);
-//        llist.insertNode(10);
-//        llist.insertNode(11);
-        //    reversePrint(llist.head);
-        //int res = getNode(llist.head, 2);
-//        printLinkedList(res);
-//        System.out.println(res);
-//
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist1.insertNode(llist1Item);
+            }
+
+            SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
+            }
+
+            SinglyLinkedListNode ptr1 = llist1.head;
+            SinglyLinkedListNode ptr2 = llist2.head;
+
+            for (int i = 0; i < llist1Count; i++) {
+                if (i < index) {
+                    ptr1 = ptr1.next;
+                }
+            }
+
+            for (int i = 0; i < llist2Count; i++) {
+                if (i != llist2Count-1) {
+                    ptr2 = ptr2.next;
+                }
+            }
+
+            ptr2.next = ptr1;
+
+            int result = findMergeNode(llist1.head, llist2.head);
+            System.out.println(result);
+
+        }
+
+
 
         scanner.close();
     }
+
+//    public static void main(String[] args) throws IOException {
+//
+//        SinglyLinkedList head = new SinglyLinkedList();
+//
+//        head.insertNode(1);
+//        head.insertNode(2);
+//        head.insertNode(3);
+//
+//        SinglyLinkedList head2 = new SinglyLinkedList();
+//
+//
+//        head2.insertNode(1);
+//        head2.insertNode(2);
+//        head2.insertNode(3);
+//        System.out.println(findMergeNode(head.head, head2.head));
+////        SinglyLinkedListNode singlyLinkedListNode = mergeLists(head.head, head2.head);
+////        printLinkedList(singlyLinkedListNode);
+////        llist.insertNode(9);
+////        llist.insertNode(10);
+////        llist.insertNode(11);
+//        //    reversePrint(llist.head);
+//        //int res = getNode(llist.head, 2);
+////        printLinkedList(res);
+////        System.out.println(res);
+////
+//
+//        scanner.close();
+//    }
 }
 
 
